5ae968f4a90be3d6c5293128ecaf2b28ae872e17
Jules Nsenda <jules.nsenda@gmail.com>
2022-06-23 10:18:33 +0200

    Add isValidUsername method

---
+++ valid-username/src/main/java/com/example/UsernameValidator.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UsernameValidator
+{
+    private UsernameValidator()
+    {
+    }
+
+    public static boolean isValidUsername(String username)
+    {
+        String pattern = "^[aA-zZ]\\w{7,29}$";
+        Matcher matcher = Pattern.compile(pattern).matcher(username);
+        return matcher.find();
+    }
+}
