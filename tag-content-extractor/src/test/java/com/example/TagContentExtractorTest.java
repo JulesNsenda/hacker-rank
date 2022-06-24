58b452c78ac56d52ca7320c5849b07955312250e
Jules Nsenda <jules.nsenda@gmail.com>
2022-06-24 12:38:19 +0200

    Added more tests

--- tag-content-extractor/src/test/java/com/example/TagContentExtractorTest.java
+++ tag-content-extractor/src/test/java/com/example/TagContentExtractorTest.java
@@ -10,5 +10,8 @@ public class TagContentExtractorTest
     public void test_getContent()
     {
         assertEquals("this is testing", TagContentExtractor.getContent("<h1>this is testing</h1>").orElse("None"));
+        assertEquals("Nayeem loves counseling", TagContentExtractor.getContent("<h1>Nayeem loves counseling</h1>").orElse("None"));
+        assertEquals("None", TagContentExtractor.getContent("<Amee>safat codes like a ninja</amee>").orElse("None"));
+        assertEquals("Imtiaz has a secret crush", TagContentExtractor.getContent("<SA premium>Imtiaz has a secret crush</SA premium>").orElse("None"));
     }
 }
