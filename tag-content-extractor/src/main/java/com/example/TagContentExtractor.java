57356426f79fefcabb720d88faea8c929516a309
Jules Nsenda <jules.nsenda@gmail.com>
2022-06-27 11:02:08 +0200

    use regex instead

--- tag-content-extractor/src/main/java/com/example/TagContentExtractor.java
+++ tag-content-extractor/src/main/java/com/example/TagContentExtractor.java
@@ -1,6 +1,7 @@
 package com.example;
 
-import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TagContentExtractor
 {
@@ -8,48 +9,21 @@ public class TagContentExtractor
     {
     }
 
-    public static Optional<String> getContent(String text)
+    public static String getContent(String text)
     {
-        String startTag = getStartTag(text).orElse("");
-        String endTag = getEndTag(text).orElse("");
+        boolean matchFound = false;
+        Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
+        Matcher m = r.matcher(text);
 
-        if (!isCorrespondingEndTag(startTag, endTag)){
-            return Optional.empty();
+        String content = "";
+        while (m.find()){
+            content = m.group(2);
+            matchFound = true;
         }
 
-        return Optional.of(text.substring(text.indexOf(startTag) + startTag.length(), text.lastIndexOf(endTag)));
-    }
-
-    private static boolean isCorrespondingEndTag(String startTag, String endTag)
-    {
-        if (!endTag.contains("/")){
-            return false;
+        if (!matchFound){
+            content = "None";
         }
-
-        endTag = endTag.replaceAll("/", "");
-
-        return startTag.equals(endTag);
-    }
-
-    private static Optional<String> getStartTag(String text)
-    {
-        if (!text.contains("<")){
-            return Optional.empty();
-        }
-
-        String tag = text.substring(text.indexOf('<'), text.indexOf('>') + 1);
-
-        return !tag.isEmpty() ? Optional.of(tag) : Optional.empty();
-    }
-
-    private static Optional<String> getEndTag(String text)
-    {
-        if (!text.contains("</")){
-            return Optional.empty();
-        }
-
-        String tag = text.substring(text.lastIndexOf("</"), text.lastIndexOf('>') + 1);
-
-        return !tag.isEmpty() ? Optional.of(tag) : Optional.empty();
+        return content;
     }
 }
--- tag-content-extractor/src/test/java/com/example/TagContentExtractorTest.java
+++ tag-content-extractor/src/test/java/com/example/TagContentExtractorTest.java
@@ -9,10 +9,10 @@ public class TagContentExtractorTest
     @Test
     public void test_getContent()
     {
-        assertEquals("this is testing", TagContentExtractor.getContent("<h1>this is testing</h1>").orElse("None"));
-        assertEquals("Nayeem loves counseling", TagContentExtractor.getContent("<h1>Nayeem loves counseling</h1>").orElse("None"));
-        assertEquals("None", TagContentExtractor.getContent("<Amee>safat codes like a ninja</amee>").orElse("None"));
-        assertEquals("Imtiaz has a secret crush", TagContentExtractor.getContent("<SA premium>Imtiaz has a secret crush</SA premium>").orElse("None"));
-        assertEquals("Sanjay has no watch\nSo wait for a while", TagContentExtractor.getContent("<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>").orElse("None"));
+        assertEquals("this is testing", TagContentExtractor.getContent("<h1>this is testing</h1>"));
+        assertEquals("Nayeem loves counseling", TagContentExtractor.getContent("<h1>Nayeem loves counseling</h1>"));
+        assertEquals("None", TagContentExtractor.getContent("<Amee>safat codes like a ninja</amee>"));
+        assertEquals("Imtiaz has a secret crush", TagContentExtractor.getContent("<SA premium>Imtiaz has a secret crush</SA premium>"));
+        assertEquals("So wait for a while", TagContentExtractor.getContent("<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>"));
     }
 }
